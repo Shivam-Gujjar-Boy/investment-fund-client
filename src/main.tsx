7195bf4c2e0a6da39d7c8dcc890e6a317be44e15
Shivam Panwar <22b3965@iitb.ac.in>
2025-06-07 13:39:35 +0000

    resolving Buffer issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { WalletContextProvider } from './context/WalletContextProvider.tsx';
 import App from './App.tsx';
+import { Buffer } from 'buffer';
 import './index.css';
 
+window.Buffer = Buffer;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <WalletContextProvider>
