13b0a8a326ef08a7b685c5211acab59f0e10d268
Shivam Panwar <22b3965@iitb.ac.in>
2025-06-07 14:09:23 +0000

    resolving buffer issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import {NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill'
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +18,12 @@ export default defineConfig({
         }),
       ],
     },
+    include: ['buffer'],
     exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
   },
 });
